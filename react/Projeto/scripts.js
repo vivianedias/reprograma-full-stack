fab223b6b374f7e33ef59dbd0374f87adbf9b3f3
vivianedias <vivipirolad@gmail.com>
2018-10-22 21:57:58 -0300

    cont. react intro-fazendo as mesmas operacoes de antes, agora com react

--- react/Projeto/scripts.js
+++ react/Projeto/scripts.js
@@ -1,4 +1,4 @@
-const linkCriarUmaConta = document.createElement('a')
+/* const linkCriarUmaConta = document.createElement('a')
 linkCriarUmaConta.className = 'link'
 linkCriarUmaConta.href = '/conta'
 linkCriarUmaConta.appendChild(document.createTextNode('Criar uma conta'))
@@ -8,15 +8,21 @@ fazerLogin.className = 'link'
 fazerLogin.href = '/login'
 fazerLogin.appendChild(document.createTextNode('Fazer login'))
 
-const divisaoProjeto = document.getElementById('projeto')
-
 const form = document.createElement('form')
 form.appendChild(linkCriarUmaConta)
 form.appendChild(fazerLogin)
-divisaoProjeto.appendChild(form)
 
+divisaoProjeto.appendChild(form)*/
 
+//React.createElement(Nome da tag, atributos que esperamos que a tag receba)
+const linkCriarUmaConta = React.createElement('a', {className: 'link', href: '/conta', children: 'Criar uma conta'})
+const fazerLogin = React.createElement('a', {className: 'link', href: '/login', children: 'Fazer Login'})
 
+const formularioLogin = React.createElement('form', {
+    children:[linkCriarUmaConta, fazerLogin]
+})
 
-
-
+//React tem um DOM próprio, a biblioteca do REACT só faz possível que nós criemos elementos. O React DOM faz uma conexão com o DOM real.
+//só aceita dois parâmetros - ReactDOM.render(elementReact, tagHTML)
+const divisaoProjeto = document.getElementById('projeto')
+ReactDOM.render(formularioLogin, divisaoProjeto)
