116c180dac92bf8a1adcad8265e3b2c282f50e14
vivianedias <vivipirolad@gmail.com>
2018-10-23 21:21:58 -0300

    aula 2 react - usando funcoes para criar tags

--- react/Projeto/scripts.js
+++ react/Projeto/scripts.js
@@ -23,6 +23,7 @@ const formularioLogin = React.createElement('form', {
     children:[linkCriarUmaConta, fazerLogin]
 })*/
 
+/*
 const linkCriarUmaConta = <a class="link" href="/conta">Criar uma conta</a>
 const fazerLogin = <a class="link" href="/login">Fazer Login</a>
 
@@ -38,3 +39,40 @@ const formularioLogin = (
 const divisaoProjeto = document.getElementById('projeto')
 
 ReactDOM.render(formularioLogin, divisaoProjeto)
+*/
+
+function Link(props){
+    return(
+        <a className="link" href={props.url}>
+            {props.children}
+        </a>
+    )   
+}
+
+//<button id="adicionar-aluno" class="botao bto-principal">Adicionar</button>
+function Botao(props){
+    let classes = 'botao'
+
+    if (props.desabilitado){
+        classes += " botao--desabilitado"
+    }
+    return (
+        <input className={classes} type="submit" value={props.children}/>
+    )
+}
+
+const paginaLogin = (
+    <main className="pagina-login">
+        <h1>Login</h1>
+        <p>Entre com seu e-mail e senha.</p>
+        <Link url="/conta">Criar uma conta</Link>
+        <Link url="/login">Fazer login</Link>
+        <Botao>Enviar</Botao>
+        <Botao desabilitado={true}>Enviar</Botao>
+    </main>
+)
+
+
+
+const divisaoProjeto = document.getElementById('projeto')
+ReactDOM.render(paginaLogin, divisaoProjeto)
