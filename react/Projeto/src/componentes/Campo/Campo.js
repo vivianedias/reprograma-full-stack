2d6a737d2d2dc993cc8f4d8cbf7d3587db466aa0
vivianedias <vivipirolad@gmail.com>
2018-10-26 21:44:13 -0300

    final da semana - campo obrigatorio login

--- react/Projeto/src/componentes/Campo/Campo.js
+++ react/Projeto/src/componentes/Campo/Campo.js
@@ -4,24 +4,26 @@ import './Campo.css'
 class Campo extends React.Component {
   constructor(props){
     super(props)
-      this.state ={
-        erro: 'Campo Obrigatório'
-      }
-    }
-    
-    valida = (evento) => {
-      const alvo = evento.target
-      if (this.props.obrigatorio === '' && alvo.value.trim() === ''){
-        const state = {
-          erro: 'Campo Obrigatório'
-        }
-      }
-      this.setState(state)
+      this.state = { erro: "" } 
     }
 
-    render () {
-      return (
-        <div>
+  valida = (evento) => {
+    const input = evento.target
+
+    if(this.props.required && input.value.trim() === ''){
+      this.setState({erro: 'Campo Obrigatório'})
+    } else if(this.props.minLength && input.value.length < this.props.minLength ){
+        this.setState({erro: `Digite pelo menos ${this.props.minLength} caracteres`})
+    } else{
+      this.setState({erro: ''})
+    }
+  }
+
+  render() {
+    console.log('Quero ver se o render foi chamado')
+    console.log(`this.props do campo ${this.props.name}`, this.props)
+    return (
+      <div>
         <input 
           id={this.props.id}
           className="campo"
@@ -29,14 +31,13 @@ class Campo extends React.Component {
           name={this.props.name}
           placeholder={this.props.placeholder}
           onChange={this.valida}
+
         />
-    
-        <p className="grupo__erro">
-          {this.state.erro}
-        </p>
-        </div>
-      )
-    }
+
+      <p className="grupo__erro">{this.state.erro}</p>  
+      </div>
+    )
   }
+}
 
 export default Campo
--- react/Projeto/src/paginas/Conta/Conta.js
+++ react/Projeto/src/paginas/Conta/Conta.js
@@ -12,16 +12,16 @@ function Conta() {
       <p>Envie o formulário para criar uma conta!</p>
       
       <Legenda htmlFor="nome">Nome:</Legenda>
-      <Campo id="nome"type="text" name="nome" placeholder="Nome"/>
+      <Campo id="nome"type="text" name="nome" placeholder="Nome" obrigatorio/>
 
       <Legenda htmlFor="telefone">Telefone:</Legenda>
-      <Campo id="telefone"type="tel" name="telefone" placeholder="Telefone"/>
+      <Campo id="telefone"type="tel" name="telefone" placeholder="Telefone" obrigatorio/>
 
       <Legenda htmlFor="email">Email:</Legenda>
-      <Campo id="email"type="email"name="email"placeholder="Email"/>
+      <Campo id="email"type="email"name="email"placeholder="Email" obrigatorio/>
       
       <Legenda htmlFor="senha">Senha:</Legenda>
-      <Campo id="senha"type="password"name="senha"placeholder="Senha"/>
+      <Campo id="senha"type="password"name="senha"placeholder="Senha" obrigatorio/>
       
       <Botao desabilitado>Enviar</Botao>
 
